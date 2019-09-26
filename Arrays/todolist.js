fa405062d70f8e078ad3e51ecc9a12192ee35ee7
biancuda11 <biancuda11@gmail.com>
2019-09-25 22:18:40 -1000

    array stuff

--- Arrays/todolist.js
+++ Arrays/todolist.js
@@ -5,12 +5,16 @@ let input = prompt("what would you like to do?")
 
 while (input !== 'quit') {
   if (input ==='list') {
-    console.log(todos);
+    todos.forEach(function(todo, i){
+      console.log(i + ': '+ todo);
+    })
   } else if (input ==='new') {
     let addition = prompt("Enter New To-do");
     todos.push(addition)
   }
   input = prompt("what would you like to do?")
+} else if (input === "delete") {
+  index = prompt("enter index of todo to delete");
 }
 
 console.log("okay, you quit the app");
