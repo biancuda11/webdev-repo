8df61659b29b2eed8714fb22df7e6fdfb6145c10
Bianculli <Dan.Bianculli@us.gt.com>
2020-12-30 09:56:55 -1000

    async js

---
+++ asynchJS/app.js
@@ -0,0 +1,26 @@
+
+// setTimeout(() => {
+//     document.body.style.backgroundColor  = 'red';
+//     setTimeout(() => {
+//         document.body.style.backgroundColor  = 'orange';
+//     }, 1000)
+// }, 1000)
+
+const delayedColorChange = (newColor, delay, doNext) => {
+    setTimeout(() => {
+        document.body.style.backgroundColor = newColor;
+        doNext && doNext();
+    }, delay)
+}
+
+delayedColorChange('red', 1000, () => {
+    delayedColorChange('orange', 1000, ()=>{
+        delayedColorChange('yellow', 1000, ()=>{
+            delayedColorChange('green', 1000, ()=>{
+                delayedColorChange('blue', 1000, ()=>{
+        
+                })
+            })
+        })
+    })
+});
